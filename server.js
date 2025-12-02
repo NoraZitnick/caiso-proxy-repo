180ca0aa75311943ae49a5433d6f6ac560a704bf
Nora <nizcat5@gmail.com>
2025-12-02 15:14:38 -0800

    idk bro

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(cors());
+app.options("*", cors());
 
 
 app.get('/caiso-csv', async (req, res) => {
